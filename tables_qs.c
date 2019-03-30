320e885e04d491f801986e7ac13fcbaccb4df09c
vikraman22 <38317246+vikraman22@users.noreply.github.com>
2019-03-30 21:10:18 +0530

    Update tables_qs.c

--- tables_qs.c
+++ tables_qs.c
@@ -1,119 +1,50 @@
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <conio.h>
-#include <stdlib.h>
-using namespace std;
-class stud
+#include <stdio.h>
+int binsch();
+void main()
 {
-public:
-    char name[20];
-    int regno;
+    int n, m, arr[10], arr1[10], i, j;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+        scanf("%d ", &arr[i]);
+    scanf("%d", &m);
 
-};
-
-int main()
-{
-    stud s[50], t[20];
-    int ch, i, n;
-    fstream fp1, fp2;
-    do
+    for(j = 0; j < m; j++)
     {
-        std::cout << " insrtion " << endl;
-        std::cout << "deletion " << endl;
-        std::cout << "updation " << endl;
-        std::cout << "exit " << endl;
-        std::cout << "choice " << endl;
-        std::cin >> ch;
-        switch(ch)
-        {
-        case 1:
-            cout << "creation " << endl;
-            fp1.open("old", ios::out | ios :: in );
-            cout << "enter the no of studs " << endl;
-            cin >> n;
-            for(i = 1; i <=n; i++)
-            {
-                cout << "regno:" << endl;
-                cin >> s[i].regno;
-                cout << "name" << endl;
-                cin >> s[i].name;
-                fp1.write((char*)&s[i], sizeof(s[i]));
-            }
-            fp1.close();
-            cout << "no of recds " << endl;
-            fp1.open("old", ios::in);
-            for(i = 1; i <=n; i++)
-            {
-                fp1.read((char*)&s[i], sizeof(s[i]));
-                cout << "regno. " << s[i].regno;
-                cout << "name is" << s[i].name << endl;
+        scanf("%d\n", &arr1[j]);
+    }
 
-            }
-            fp1.close();
-            break;
+     binsch(arr, 0, n-1, &arr1);
 
-        case 2:
-            int deletion;
-            fp1.open("old", ios::in);
-            fp2.open("new", ios::out);
-            cout << "deletion of records " << endl;
-            cout << "enter regno" << endl;
-            cin >> deletion;
-            for(i = 1; i <=n; i++)
-            {
-                fp1.read((char*)&s[i], sizeof(s[i]));
-                if(s[i].regno != deletion)
-                fp2.write((char*)&s[i], sizeof(s[i]));
+}
 
-            }
-            n--;
-            fp1.close();
-            fp2.close();
-            fp1.close("old", ios::out);
-            fp2.open("new", ios::in);
-            cout << "\nAfter deletion" << endl;
-            for(i = 1; i <=n; i++)
-            {
-                fp2.read((char*)&s[i], sizeof(s[i]));
-                cout << "\n\t" << s[i].regno << endl;
-                cout << "\n\t" << s[i].name << endl;
-                fp1.write((char*)&s[i], sizeof(s[i]));
+int binsch(int arr[], int l, int r, int* arr2[])
+{
+    int j;
+    if(r > l)
+    {
+        int mid = (l + r) / 2;
+        int size = sizeof((int*)arr2)/sizeof((int*)arr2[0]);
+        int count = 0;
+        for(j = 0; j < size; j++)
+         {
+             if(arr[mid] == (int*)arr2[j])
+             {
+                 count++;
+                 printf("%d", count);
 
-            }
-            fp1.close();
-            fp2.close();
-            break;
-
-        case 3:
-            cout << "\nmodification of record" << endl;
-            fp2.open("old", ios::out | ios::app);
-            cout << "\n Enter the regno " << endl;
-            cin >> s[i].regno;
-            cout << "enter name " << endl;
-            cin >> s[i].name;
-            for(i = 1; i<=n; i++)
-            fp2.write((char*)&s[i],sizeof(s[i]));
-            n++;
-            fp2.close();
-            cout << "new modification " << endl;
-            fp2.open("old", ios::in);
-            fp2.open("new", ios::in);
-            for(i =1 ; i <= n; i++)
-            {
-                fp2.read((char*)&s[i],sizeof(s[i]));
-                fp1.write((char*)&s[i],sizeof(s[i]));
-                cout << "\n\t" << s[i].regno << "\n\t" << s[i].name;
-
-            }
-            fp1.close();
-            fp2.close();
-            break;
+             }
 
 
+             else if(arr[mid] > (int*)arr2[j])
+                return binsch(arr, l, mid-1, arr2);
+
+             else if(arr[mid] < (int*)arr2[j])
+                 return binsch(arr, mid+1, r, arr2);
+
+             else
+                printf("NOT PRESENT");
 
         }
+        return 0;
     }
-    while(ch < 4);
-    return 0;
-}
+

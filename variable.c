76404795bdeb9e3d90d11481767713b1c8e570cf
vikraman22 <38317246+vikraman22@users.noreply.github.com>
2019-04-14 19:13:09 +0530

    inschofeasy.c

--- variable.c
+++ variable.c
@@ -3,18 +3,34 @@ int check(int arr[], int n)
 {
     int i;
     int k = 1;
+    int m = 2;
+
+        if(n == 1)
+        {
+            if(arr[0] == 0)
+            {
+                return m;
+            }
+        }
+
+
         for(i = 0; i < n; i++)
         {
-           if(arr[i] == k)
-          {
-              break;
-              return 1;
-          }
+        if(arr[i] == 0)
+        {
+              continue;
+              return m;
 
         }
+        else
+        {
+            return k;
+        }
+
+}
 }
 
-void main()
+int main()
 {
     int i, n, arr[100], l;
     scanf("%d", &n);
@@ -24,8 +40,10 @@ void main()
     }
     l = check(arr, n);
     if(l == 1)
-        printf("HARD");
+    {
+         printf("HARD");
+    }
     else
         printf("EASY");
-
+    return 0;
 }
